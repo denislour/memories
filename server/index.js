9587bf6df1f30324273ef1a49f89bf1302c3eea7
Lektor <denislour@gmail.com>
2022-07-14 00:47:58 +0700

    [BE] - Create routes, models, controllers for post model.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,6 +19,6 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => console.log(`Server runnin on port ${PORT}`))
+    app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
   )
   .catch((error) => console.log(error.message));
