e37373b960204b7d9a9d4b75fa8385c2a96f389d
Lektor <denislour@gmail.com>
2022-07-13 23:53:52 +0700

    [BE] - initial project.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors);
+
+const CONNECTION_URL = "mongodb://localhost:27017/memo";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runnin on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
